3219cd894e286370d7720a5e4c3289a009a82273
Pascal Jaeger <pascal.jaeger@kleinknecht.de>
2021-07-05 15:10:02 +0200

    cleaned code

--- RemoteObject.cs
+++ RemoteObject.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Reflection;
-
 namespace TIAAutoSave
 {
     public class RemoteObject : MarshalByRefObject
--- TIAAutosaveForm.cs
+++ TIAAutosaveForm.cs
@@ -1,15 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.IO;
-using System.Linq;
 using System.Runtime.Remoting.Channels.Ipc;
-using System.Security.Permissions;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using Siemens.Engineering;
 
